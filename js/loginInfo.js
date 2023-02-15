6be367549452e292e236e7957212de55131d7184
AllynnAlvarico <allynn_alvarico@yahoo.com>
2023-02-15 14:54:32 +0000

    Added Registration Form, fixes the landing page and added a animation on the 'Register now'

--- js/loginInfo.js
+++ js/loginInfo.js
@@ -8,19 +8,16 @@ var userPassword = "kateTabi";
 function submit_login(){
 	var userEmail = document.getElementById("userInfo").value;
 	var userPass = document.getElementById("userInPwd").value;
-	console.log(userEmail);
-	console.log(userPass);
 	if (userLogin == userEmail) {
 		if (userPassword == userPass){
-			console.log("Im here");
-			var url = "testChart.html";
+			var url = "userAccDash.html";
 			window.location.replace(url);
 			return false;
 		}
 	} else{
 			window.alert("Wrong Password user:admin password:kateTabi");
-			var url = "index.html";
-			window.location.replace(url)
+			var main = "index.html";
+			window.location.replace(main)
 			return false;
 		}
 		
